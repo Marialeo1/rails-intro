0aadc43844cc5b10c1f4e708aa1e8650f9fffa18
Maria Varela <leonor.varela91330@gmail.com>
2022-08-12 23:38:54 +0200

    lists controllers

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,8 @@
+class ListsController < ApplicationController
+
+  # A list of the repositories created in the last 7 days with the most number of stars in GitHub should be displayed
+  # and the user should be able to favourite them
+  # The favourited repositories should be visible either through a filter or
+  # in a different tab. Some basic info about the repo should be displayed, such as: repo name,
+  #  link to GitHub, description and number of stars.
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,24 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'lists#index'
 end
+
+
+# Rails.application.routes.draw do
+
+#   devise_for :users
+#   get '/dashboard', to: 'pages#dashboard'
+#   post '/dashboard', to: 'pages#addaddress'
+#   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+#   resources :pets do
+#     resources :bookings, except: [:show, :edit, :update]
+#   end
+#   resources :booking, only: [] do
+#     resources :reviews, only: :create
+#   end
+#   resources :bookings, only: [:show, :edit, :update]
+
+#   get 'booking/:id/validate', to: 'bookings#validate'
+#   get 'booking/:id/decline', to: 'bookings#decline'
+
+# end
